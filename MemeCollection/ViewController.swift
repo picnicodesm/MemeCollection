25bf59c4e4c40d316da494f7b629a80c1541004b
Sangmin Kim <offiksm0903@naver.com>
2024-10-14 20:38:34 +0900

    Initial Commit

---
+++ MemeCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeCollection
+//
+//  Created by 김상민 on 10/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
