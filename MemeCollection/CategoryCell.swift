8af2f95d7e136d2274009844ab0e31428ccc1aff
Sangmin Kim <offiksm0903@naver.com>
2024-10-14 22:21:17 +0900

    feat: Add CollectionView in the MainView

---
+++ MemeCollection/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  MemeCollection
+//
+//  Created by 김상민 on 10/14/24.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    static let identifier = "CategoryCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        
+    }
+}
--- MemeCollection/MainViewController.swift
+++ MemeCollection/MainViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    let categories = ["Favorites", "Category 1", "Category 2", "Category 3"]
+    
+    typealias Item = String
+    enum Section {
+        case main
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
+        configureDataSource()
         configureNavigationBarItem()
         configureToolbar()
     }
@@ -20,10 +30,49 @@ class MainViewController: UIViewController {
 }
 
 extension MainViewController {
+    private func configureDataSource() {
+        collectionView.register(UICollectionViewListCell.self, forCellWithReuseIdentifier: "CollectionViewListCell")
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewListCell", for: indexPath) as? UICollectionViewListCell else { return UICollectionViewCell() }
+            
+            var config = cell.defaultContentConfiguration()
+            config.text = item
+            
+            cell.contentConfiguration = config
+            
+            return cell
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(categories)
+        dataSource.apply(snapshot)
+    }
+}
+
+
+extension MainViewController: UICollectionViewDelegate {
+    
+}
+
+
+extension MainViewController {
     private func configureView() {
         view.backgroundColor = .white
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationItem.title = "MemeCollection"
+        
+        var configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        configuration.headerMode = .none
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = dataSource
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(collectionView)
     }
     
     private func configureNavigationBarItem() {
@@ -48,6 +97,6 @@ extension MainViewController {
         let flexibleSpaceBarButtonItem = UIBarButtonItem(systemItem: .flexibleSpace)
         
         self.toolbarItems = [newReminderBarButtonItem, flexibleSpaceBarButtonItem, addListBarButtonItem]
-
     }
+
 }
